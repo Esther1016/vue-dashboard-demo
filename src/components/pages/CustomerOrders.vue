44cdc5f44af3af5406b96f7f64816be3a89c0b41
EstherChen000 <estherchen000@gmail.com>
2020-12-01 17:29:47 +0800

    選購產品及加入購物車(add 列表)

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -55,6 +55,46 @@
         </div>
       </div>
     </div>
+    <div class="my-5 row justify-content-center">
+      <div class="my-5 row justify-content-center">
+        <table class="table">
+          <thead>
+            <th></th>
+            <th>品名</th>
+            <th>數量</th>
+            <th>單價</th>
+          </thead>
+          <tbody>
+            <tr v-for="item in cart.carts" :key="item.id" v-if="cart.carts">
+              <td class="align-middle">
+                <button type="button" class="btn btn-outline-danger btn-sm">
+                  <i class="far fa-trash-alt"></i>
+                </button>
+              </td>
+              <td class="align-middle">
+                {{ item.product.title }}
+                <!-- <div class="text-success" v-if="item.coupon">
+                  已套用優惠券
+                </div> -->
+              </td>
+              <td class="align-middle">{{ item.qty }}/{{ item.product.unit }}</td>
+              <td class="align-middle text-right">{{ item.final_total }}</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="3" class="text-right">總計</td>
+              <td class="text-right">{{ cart.total }}</td>
+            </tr>
+            <!-- <tr v-if="cart.final_total">
+              <td colspan="3" class="text-right text-success">折扣價</td>
+              <td class="text-right text-success">{{ cart.final_total }}</td>
+            </tr> -->
+          </tfoot>
+        </table>
+      </div>
+    </div>
+    <!-- modal -->
     <div
       class="modal fade"
       id="productModal"
@@ -135,6 +175,7 @@ export default {
       status: {
         loadingItem: ""
       },
+      cart:{},
       isLoading: false
     };
   },
@@ -181,6 +222,7 @@ export default {
       vm.isLoading = true;
       this.$http.get(api).then(response => {
         console.log(response.data);
+        vm.cart = response.data.data;
         vm.isLoading = false;
         // vm.products = response.data.products;
       });
